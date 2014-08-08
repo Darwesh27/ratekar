a9a0296788e3fc21385e10c95d22585541b0a6da
Darwesh <malik.junaid27@gmail.com>
2014-08-08 19:29:17 +0500

    Structure: basic structure added with no models

--- ratekar/settings.py
+++ ratekar/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'personality',
+	'social',
+	'timeline',
+	'api',
+	'web',
 )
 
 MIDDLEWARE_CLASSES = (
