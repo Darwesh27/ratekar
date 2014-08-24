8d35dcc50ca3568e1783f5b10c44be7d4584e0c5
Darwesh <malik.junaid27@gmail.com>
2014-08-24 21:59:38 +0500

    Friendship.status added

--- social/models.py
+++ social/models.py
@@ -8,6 +8,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class UserManager(BaseUserManager):
 	def create_user(self, username, email, firstname, lastname, password = None):
 		user = self.model(username = username, email = email, firstname = firstname, lastname = lastname)
+		user.set_password(password)
 		return user
 
 	def create_superuser(self, username, email, firstname, lastname, password):
@@ -32,6 +33,9 @@ class User(AbstractBaseUser):
 	def get_full_name(self):
 		return self.firstname + " " + self.lastname
 
+	def __unicode__(self):
+		return self.get_full_name()
+
 
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = ['firstname', 'lastname', 'email']
@@ -50,6 +54,8 @@ class Friendship(models.Model):
 	lists = models.ManyToManyField(Friendslist)
 	circle = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(4)], default = 2)
 	nick = models.CharField(max_length=50, null = True)
+	status = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(4)], default = 1)
+
 
 
 
