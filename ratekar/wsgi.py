e9917cc69aba41998de5fab82c175e1a20ae4be4
Darwesh <malik.junaid27@gmail.com>
2015-01-15 19:48:57 +0500

    added sys.path.append

--- ratekar/wsgi.py
+++ ratekar/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+
+sys.path.append('/home/malik_junaid27_gmail_com/Darwesh/rateker')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ratekar.settings")
 
 from django.core.wsgi import get_wsgi_application
